3d9b501e001d8c5266c8d302a5895cbe6e5ac3fb
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2023-06-17 11:21:34 +0100

    Fix download_release

--- lib/utils.bash
+++ lib/utils.bash
@@ -3,42 +3,43 @@
 set -euo pipefail
 
 TOOL_NAME="awscli-local"
-TOOL_TEST="awslocal --version"
+BASE_TOOL="awslocal"
+TOOL_TEST="$BASE_TOOL --version"
 
 fail() {
-	echo -e "asdf-$TOOL_NAME: $*"
+	printf "asdf-$TOOL_NAME: $*\n"
 	exit 1
 }
 
 list_all_versions() {
 	current_script_path=${BASH_SOURCE[0]}
 	plugin_dir=$(dirname "$(dirname "$current_script_path")")
-	v="${plugin_dir}/versions.txt"
+	v="$plugin_dir/versions.txt"
 
 	set +e
-	pip3 install --user awscli-local== 2>"${v}"
+	pip3 install --user awscli-local== 2>"$v"
 	set -e
 	sed -i 's/.*from versions: //g' "$v"
 	sed -i 's/)//g' "$v"
 	sed -i 's/,//g' "$v"
-	grep -v ERROR "$v" | cat | awk -F' ' '{ for (i = 1; i <= NF; i++) print $i }' >"$v.new"
+	grep -v ERROR "$v" > "$v.new"
 	cat "$v.new"
 }
 
 download_release() {
 	local version filename
 	version="$1"
-	filename="$2"
+	dirname=$(dirname "$2")
+	filename=$(basename "$2")
 
-	echo "* Downloading $TOOL_NAME release $version..."
-	dest=$(dirname filename)
-	pip3 download --dest "$dest" "$filename" || fail "Could not download from pip"
+	printf "* Downloading $TOOL_NAME release $version...\n"
+	pip3 download --dest "$dirname" "$filename==$version" || fail "Could not download from pip"
 }
 
 install_version() {
 	local install_type="$1"
 	local version="$2"
-	local install_path="${3%/bin}/bin"
+	local install_path="$3"
 
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
@@ -49,12 +50,51 @@ install_version() {
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
 		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		tool_cmd="$(printf "$TOOL_TEST\n" | cut -d' ' -f1)"
+		chmod +x "$install_path/bin/$BASE_TOOL"
 
-		echo "$TOOL_NAME $version installation was successful!"
+		install_localstack "$install_path"
+		unpack_deps "$install_path"
+
+		[ -x "$install_path/bin/$tool_cmd" ] || fail "Expected $install_path/$tool_cmd to be executable."
+
+		printf "$TOOL_NAME $version installation was successful!\n"
 	) || (
 		rm -rf "$install_path"
 		fail "An error occurred while installing $TOOL_NAME $version."
 	)
+}
+
+install_localstack() {
+	install_path="$1"
+
+	localstack_client_tar_gz=$(find "$install_path" -name "localstack-client*")
+	tar zxf "$localstack_client_tar_gz" --strip-components=1 -C "$install_path"
+
+	localstack_client=$(find "$install_path" -name "localstack_client")
+	mv "$localstack_client" "$install_path/bin"
+}
+
+unpack_deps() {
+	install_path="$1"
+
+	unpack_dep "$install_path" boto3 boto3
+	unpack_dep "$install_path" botocore botocore
+	unpack_dep "$install_path" jmespath jmespath
+	unpack_dep "$install_path" python_dateutil dateutil
+	unpack_dep "$install_path" s3transfer s3transfer
+	unpack_dep "$install_path" six six.py
+	unpack_dep "$install_path" urllib3 urllib3
+}
+
+unpack_dep() {
+	install_path="$1"
+	dep="$2"
+	dep_name="$3"
+
+  dep_whl=$(find "$install_path" -name "$dep-*")
+  wheel3 unpack "$dep_whl" --dest "$install_path"
+
+  dep=$(find "$install_path" -depth 2 -name "$dep_name")
+  mv "$dep" "$install_path/bin"
 }
